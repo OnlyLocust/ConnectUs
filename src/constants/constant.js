fdd740a7ce597088508fad589067b26135550bef
Harsh Kr <harshchudasama4739s@gmail.com>
2025-06-10 21:35:24 +0530

    chat notification added 2

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,3 +1,4 @@
+import  dotenv from 'dotenv'
+dotenv.config()
 
-
-export const API_URL = "http://localhost:3000/api"; 
+export const API_URL =  process.env.API_URL || "http://localhost:3000/api"; 
--- src/store/store.js
+++ src/store/store.js
@@ -14,7 +14,7 @@ import notification from './notificationSlice'
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist:['auth' , 'recv', 'follow']
+  whitelist:['auth','recv','follow']
 };
 
 // combine reducers
