ffc452f228fdc11161dbd32632fbbbb5f7db03b9
Harsh Kr <harshchudasama4739s@gmail.com>
2026-06-10 20:49:25 +0530

    Added auth middleware

---
+++ ConnectUs_Backend/src/middleware/auth.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    req.userId = decoded.id;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
+};
