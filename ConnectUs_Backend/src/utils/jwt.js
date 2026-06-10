3ba7d9abf8f7204519c396f0fc4c191467375ca7
Harsh Kr <harshchudasama4739s@gmail.com>
2026-06-10 16:49:39 +0530

    Added token generation

--- ConnectUs_Backend/src/utils/jwt.js
+++ ConnectUs_Backend/src/utils/jwt.js
@@ -1,21 +1,19 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import jwt from "jsonwebtoken";
 
 export const generateToken = (id) => {
-    try {
+  if (!id) {
+    throw new Error("ID is required to generate a token");
+  }
 
-        if(!id) {
-            throw new Error("ID is required to generate a token");
-        }
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined");
+  }
 
-        const token =  jwt.sign({ id }, process.env.JWT_SECRET, {
-            expiresIn: '1h' // Token expires in 1 hour
-        });
-        return token;
-
-    } catch (error) {
-        return ""
+  return jwt.sign(
+    { id },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
     }
-}
+  );
+};
