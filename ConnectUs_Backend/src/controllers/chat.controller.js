ba815a368047e8dc66fd7f1ceb994a4d9ae6e27b
Harsh Kr <harshchudasama4739s@gmail.com>
2026-06-12 00:09:21 +0530

    Added chat route addchat

--- ConnectUs_Backend/src/controllers/chat.controller.js
+++ ConnectUs_Backend/src/controllers/chat.controller.js
@@ -47,3 +47,46 @@ export const getMessages = async (req, res) => {
     });
   }
 };
+
+export const addChat = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { recvId } = req.body;
+
+    if (!recvId) {
+      return res.status(400).json({
+        success: false,
+        message: "Receiver ID is required",
+      });
+    }
+
+    let chat = await Chat.findOne({
+      members: {
+        $all: [userId, recvId],
+        $size: 2,
+      },
+    }).select("_id");
+
+    if (!chat) {
+      chat = await Chat.create({
+        members: [userId, recvId],
+        notRead: {
+          [userId.toString()]: 0,
+          [recvId.toString()]: 0,
+        },
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      chatId: chat._id,
+    });
+  } catch (error) {
+    console.error("Create Chat Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- ConnectUs_Backend/src/routes/chat.routes.js
+++ ConnectUs_Backend/src/routes/chat.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getMessages } from "../controllers/chat.controller.js";
+import { getMessages, addChat } from "../controllers/chat.controller.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/:recvId", protect, getMessages);
+router.post("/addchat", protect, addChat);
 
 export default router;
