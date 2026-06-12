14f84d8f37c18fe7b2306c5f7e8206dd18093b71
Harsh Kr <harshchudasama4739s@gmail.com>
2026-06-12 23:14:05 +0530

    Added get user followers and following

--- ConnectUs_Backend/src/controllers/user.controller.js
+++ ConnectUs_Backend/src/controllers/user.controller.js
@@ -149,3 +149,41 @@ export const getSuggestions = async (req, res) => {
     });
   }
 };
+
+const getFollowData = async (
+  req,
+  res,
+  field
+) => {
+  const { userId } = req.params;
+  
+  const user = await User.findById(userId)
+    .select(`${field} username profilePicture`)
+    .populate(
+      field,
+      "username profilePicture"
+    )
+    .lean();
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    userData: {
+      username: user.username,
+      profilePicture: user.profilePicture,
+    },
+    followData: user[field],
+  });
+};
+
+export const getFollowers = (req, res) =>
+  getFollowData(req, res, "followers");
+
+export const getFollowing = (req, res) =>
+  getFollowData(req, res, "following");
--- ConnectUs_Backend/src/routes/user.routes.js
+++ ConnectUs_Backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getMe, getSuggestions, getUsers } from "../controllers/user.controller.js";
+import {getFollowers, getFollowing, getMe, getSuggestions, getUsers } from "../controllers/user.controller.js";
 import { protect } from "../middleware/auth.js"
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get("/", protect, getMe);
 router.get("/users", protect, getUsers);
 router.get("/suggestions", protect, getSuggestions);
+router.get("/followers/:userId", protect, getFollowers);
+router.get("/following/:userId", protect, getFollowing);
 
 export default router;
