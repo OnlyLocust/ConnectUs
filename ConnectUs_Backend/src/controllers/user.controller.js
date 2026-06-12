d19c865c66f2ceef85865a167d56f27dc95abf8c
Harsh Kr <harshchudasama4739s@gmail.com>
2026-06-12 19:45:22 +0530

    Added route get all users

--- ConnectUs_Backend/src/controllers/user.controller.js
+++ ConnectUs_Backend/src/controllers/user.controller.js
@@ -85,3 +85,27 @@ export const getMe = async (req, res) => {
     });
   }
 };
+
+export const getUsers = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const users = await User.find({
+      _id: { $ne: userId },
+    })
+      .select("username profilePicture")
+      .lean();
+
+    return res.status(200).json({
+      success: true,
+      users,
+    });
+  } catch (error) {
+    console.error("Get Users Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- ConnectUs_Backend/src/routes/user.routes.js
+++ ConnectUs_Backend/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getMe } from "../controllers/user.controller.js";
+import {getMe, getUsers } from "../controllers/user.controller.js";
 import { protect } from "../middleware/auth.js"
 
 const router = express.Router();
 
 router.get("/", protect, getMe);
+router.get("/users", protect, getUsers);
 
 export default router;
