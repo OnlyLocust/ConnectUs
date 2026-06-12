b47edb18117501f4110d9b63321ffa70d5289532
Harsh Kr <harshchudasama4739s@gmail.com>
2026-06-12 20:51:54 +0530

    Added route toggle bookmark

--- ConnectUs_Backend/src/routes/post.routes.js
+++ ConnectUs_Backend/src/routes/post.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getPosts } from "../controllers/post.controller.js";
+import { getPosts, toggleBookmark } from "../controllers/post.controller.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/getall", protect, getPosts);
+router.patch("/bookmark/:postId",  protect, toggleBookmark);
 
 export default router;
