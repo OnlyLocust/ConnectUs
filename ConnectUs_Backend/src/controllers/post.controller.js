21b5ca207ac6136136a94710bea1e83f2a73e520
Harsh Kr <harshchudasama4739s@gmail.com>
2026-06-13 00:12:57 +0530

    Added route to get one post

--- ConnectUs_Backend/src/controllers/post.controller.js
+++ ConnectUs_Backend/src/controllers/post.controller.js
@@ -81,3 +81,44 @@ export const toggleBookmark = async (req, res) => {
     });
   }
 };
+
+export const getPost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+
+    if (!postId) {
+      return res.status(400).json({
+        success: false,
+        message: "Post ID is required",
+      });
+    }
+
+    const post = await Post.findById(postId)
+      .populate("author", "username profilePicture")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "author",
+          select: "username profilePicture",
+        },
+      });
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Post fetched successfully",
+      post,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- ConnectUs_Backend/src/routes/post.routes.js
+++ ConnectUs_Backend/src/routes/post.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getPosts, toggleBookmark } from "../controllers/post.controller.js";
+import { getPost, getPosts, toggleBookmark } from "../controllers/post.controller.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/getall", protect, getPosts);
+router.get("/get/:postId", protect, getPost);
 router.patch("/bookmark/:postId",  protect, toggleBookmark);
 
 export default router;
