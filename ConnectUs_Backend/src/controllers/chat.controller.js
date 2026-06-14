e223ecd4ae94797b5f55aea9150ccd0f60eb6418
Harsh Kr <harshchudasama4739s@gmail.com>
2026-06-14 23:36:02 +0530

    Added route mark as not read temp1

--- ConnectUs_Backend/src/controllers/chat.controller.js
+++ ConnectUs_Backend/src/controllers/chat.controller.js
@@ -2,6 +2,7 @@ import Chat from "../models/chat.model.js";
 import Message from "../models/message.model.js";
 import { eventBus, EVENTS } from "../utils/eventBus.js";
 
+
 export const getMessages = async (req, res) => {
   try {
     const { recvId } = req.params;
@@ -49,6 +50,7 @@ export const getMessages = async (req, res) => {
     });
   }
 };
+
 
 export const addChat = async (req, res) => {
   try {
@@ -209,6 +211,69 @@ export const sendMessage = async (req, res) => {
       optimisticId,
       messageId: addMessage._id.toString(),
       createdAt: addMessage.createdAt,
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+export const markChatAsRead = async (req, res) => {
+  console.log('really?');
+  
+  try {
+
+    const chatId = req.params.id;
+    const userId = req.userId;
+
+    const chat = await Chat.findById(chatId);
+
+    if (!chat) {
+      return res.status(404).json({
+        success: false,
+        message: "Chat not found",
+      });
+    }
+
+    // Optional security check
+    if (
+      !chat.members.some(
+        member => member.toString() === userId
+      )
+    ) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied",
+      });
+    }
+
+    chat.notRead.set(userId, 0);
+
+    await chat.save();
+
+    const recvId = chat.members
+      .find(
+        member =>
+          member.toString() !== userId
+      )
+      ?.toString();
+
+    eventBus.emit(EVENTS.MESSAGE_READ, {
+      chatId,
+      userId,
+      recvId,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Marked as read",
     });
 
   } catch (error) {
--- ConnectUs_Backend/src/routes/chat.routes.js
+++ ConnectUs_Backend/src/routes/chat.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getMessages, addChat ,chatUsers, sendMessage} from "../controllers/chat.controller.js";
+import { getMessages, addChat ,chatUsers, sendMessage, markChatAsRead} from "../controllers/chat.controller.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get("/get/:recvId", protect, getMessages);
 router.post("/addchat", protect, addChat);
 router.get("/chatusers", protect, chatUsers);
 router.post("/send/:id", protect, sendMessage);
+router.patch("/notread/:id", protect, markChatAsRead);
 
 export default router;
