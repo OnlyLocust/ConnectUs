b45452aa4f98c42711ca98d22905eefe9ce99be5
Harsh Kr <harshchudasama4739s@gmail.com>
2026-06-15 18:36:39 +0530

    Added route get all post for home page

--- ConnectUs_Backend/src/controllers/post.controller.js
+++ ConnectUs_Backend/src/controllers/post.controller.js
@@ -122,3 +122,60 @@ export const getPost = async (req, res) => {
     });
   }
 };
+
+const skipValue = 4;
+
+export const getHomePosts = async (req, res) => {
+  
+  try {
+    const id = req.userId; // set by auth middleware
+
+    if (!id) {
+      return res.status(401).json({
+        message: "Unauthorized",
+        success: false,
+      });
+    }
+
+    const before = req.query.before;
+    const limit = parseInt(req.query.limit) || 4;
+
+    let query = {};
+
+    if (before) {
+      query.createdAt = {
+        $lt: new Date(before),
+      };
+    }
+
+    let queryExec = Post.find(query)
+      .populate("author", "username profilePicture")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "author",
+          select: "username profilePicture",
+        },
+      })
+      .sort({ createdAt: -1 });
+
+    if (!before) {
+      const skip = parseInt(req.query.skip) || 0;
+
+      queryExec = queryExec.skip(skip * skipValue);
+    }
+
+    const posts = await queryExec.limit(limit);
+
+    return res.status(200).json({
+      message: "Posts fetched successfully",
+      success: true,
+      posts,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
--- ConnectUs_Backend/src/routes/post.routes.js
+++ ConnectUs_Backend/src/routes/post.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getPost, getPosts, toggleBookmark } from "../controllers/post.controller.js";
+import { getHomePosts, getPost, getPosts, toggleBookmark } from "../controllers/post.controller.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/getall", protect, getPosts);
 router.get("/get/:postId", protect, getPost);
 router.patch("/bookmark/:postId",  protect, toggleBookmark);
+router.get("/", protect, getHomePosts);
 
 export default router;
