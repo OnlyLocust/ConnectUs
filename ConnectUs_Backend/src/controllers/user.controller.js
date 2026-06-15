e69429cbc0a6d238ac6f0db677bbfb9901d7b62e
Harsh Kr <harshchudasama4739s@gmail.com>
2026-06-15 18:27:33 +0530

    Added route get user profile

--- ConnectUs_Backend/src/controllers/user.controller.js
+++ ConnectUs_Backend/src/controllers/user.controller.js
@@ -297,3 +297,117 @@ export const updateProfile = async (req, res) => {
     });
   }
 };
+
+
+export const getUserProfile = async (req, res) => {
+  
+  try {
+    const profileId = req.params.id;
+
+    const objectUserId =
+      new mongoose.Types.ObjectId(profileId);
+
+    const userWithCounts = await User.aggregate([
+      {
+        $match: {
+          _id: objectUserId,
+        },
+      },
+      {
+        $lookup: {
+          from: "posts",
+          localField: "posts",
+          foreignField: "_id",
+          as: "posts",
+        },
+      },
+      {
+        $lookup: {
+          from: "posts",
+          localField: "bookmarks",
+          foreignField: "_id",
+          as: "bookmarks",
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          username: 1,
+          profilePicture: 1,
+          lastSeen: 1,
+          bio: 1,
+          gender: 1,
+
+          followerCount: {
+            $size: {
+              $ifNull: ["$followers", []],
+            },
+          },
+
+          followingCount: {
+            $size: {
+              $ifNull: ["$following", []],
+            },
+          },
+
+          posts: {
+            $map: {
+              input: "$posts",
+              as: "post",
+              in: {
+                _id: "$$post._id",
+                image: "$$post.image",
+                likeCount: {
+                  $size: "$$post.likes",
+                },
+                commentCount: {
+                  $size: "$$post.comments",
+                },
+              },
+            },
+          },
+
+          bookmarks: {
+            $map: {
+              input: "$bookmarks",
+              as: "post",
+              in: {
+                _id: "$$post._id",
+                image: "$$post.image",
+                likeCount: {
+                  $size: "$$post.likes",
+                },
+                commentCount: {
+                  $size: "$$post.comments",
+                },
+              },
+            },
+          },
+        },
+      },
+    ]);
+
+    const user = userWithCounts[0];
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    user.online =
+      global.onlineUsers?.has(profileId) || false;
+
+    return res.status(200).json({
+      success: true,
+      user,
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- ConnectUs_Backend/src/routes/user.routes.js
+++ ConnectUs_Backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getFollowers, getFollowing, getMe, getSuggestions, getUsers ,updateProfile} from "../controllers/user.controller.js";
+import {getFollowers, getFollowing, getMe, getSuggestions, getUserProfile, getUsers ,updateProfile} from "../controllers/user.controller.js";
 import { protect } from "../middleware/auth.js"
 import upload from "../middleware/multer.js";
 
@@ -11,5 +11,7 @@ router.get("/suggestions", protect, getSuggestions);
 router.get("/followers/:userId", protect, getFollowers);
 router.get("/following/:userId", protect, getFollowing);
 router.patch("/profile",  protect,  upload.single("profilePicture"),  updateProfile);
+router.get("/get/:id", protect, getUserProfile);
+
 
 export default router;
