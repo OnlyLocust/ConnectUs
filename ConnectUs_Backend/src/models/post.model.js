d6dffa73ef7fe772cdb77b5366ae6b1c524f8cef
Harsh Kr <harshchudasama4739s@gmail.com>
2026-06-17 22:42:18 +0530

    Update v1.6 : update post schema

--- ConnectUs_Backend/src/models/post.model.js
+++ ConnectUs_Backend/src/models/post.model.js
@@ -35,6 +35,8 @@ const postSchema = mongoose.Schema(
   }
 );
 
+postSchema.index({ createdAt: -1 });
+
 const Post = mongoose.models.Post || mongoose.model('Post',postSchema)
 
 export default Post
